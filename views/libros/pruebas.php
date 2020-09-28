5e69c71b9e1b9ac7fd80683e1d8c76af4736eacd
Pepe Millan Fernandez <jose.millan@iesdonana.org>
2020-09-28 22:02:11 +0200

    Archivo de pruebas webscrap

---
+++ views/libros/pruebas.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\bootstrap4\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'BookFinder';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="libros-finder">
+
+    
+
+</div>
