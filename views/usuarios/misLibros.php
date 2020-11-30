89ab1e91c93eebb2a765214d62011415f19def31
José Millán Fernández <pepemillanfernandez@gmail.com>
2020-11-30 14:21:21 +0100

    Borro gridview

--- views/usuarios/misLibros.php
+++ views/usuarios/misLibros.php
@@ -10,6 +10,7 @@ use yii\widgets\ListView;
 $this->title = 'Mis libros';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="usuarios-mis-libros">
 
     <h1><?= Html::encode($this->title) ?></h1>
@@ -47,50 +48,5 @@ $this->params['breadcrumbs'][] = $this->title;
         }
     ]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            
-            //'img',
-            'libro.titulo',
-            'libro.autor',
-            'orden',
-
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{view} {update} {delete}',
-                'buttons' => [
-                    'view' => function ($url, $model, $key) {
-                        if (Yii::$app->user->isGuest || Yii::$app->user->identity->id != $model->usuario->id) {
-                            return '';
-                        }
-                        return Html::a('', ['libros/view', 'id' => $model->libro->id], [
-                            'class' => 'glyphicon glyphicon-eye-open',
-                            'title' => 'Ver libro',
-                            'style' => [
-                                'color' => 'limegreen',
-                            ]
-                        ]);
-                    },
-                    'update' =>  function ($url, $model, $key) {
-                        return '';
-                    },
-                    'delete' => function ($url, $model, $key){
-                        if (Yii::$app->user->isGuest || Yii::$app->user->identity->id != $model->usuario->id) {
-                            return '';
-                        }
-                        return Html::a('', ['usuarios/borrar-libro', 'l' => $model->libro->id], [
-                            'class' => 'glyphicon glyphicon-trash',
-                            'title' => 'Ver libro',
-                            'style' => [
-                                'color' => 'red',
-                            ]
-                        ]);
-                    }
-                ]
-            ],
-        ],
-    ]); ?>
-
-
 </div>
+
